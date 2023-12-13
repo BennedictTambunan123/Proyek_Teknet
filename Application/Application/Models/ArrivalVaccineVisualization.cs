e059a5c3fdee198ed24944a2df2d6fa6604d8443
BennedictTambunan123 <103813550+BennedictTambunan123@users.noreply.github.com>
2023-12-13 09:10:08 +0700

    push

---
+++ Application/Application/Models/ArrivalVaccineVisualization.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Application.Models
+{
+    public class ArrivalVaccineVisualization
+    {
+        public string RS_Name { get; set; }
+        public string ProdusenName { get; set; }
+        public int Vaccine_count { get; set; }
+    }
+}
---
+++ Application/Application/Models/ProdusenVisualization.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Application.Models
+{
+    public class ProdusenVisualization
+    {
+        public string Name { get; set; }
+
+        public int ProdukCount { get; set; }
+    }
+}
